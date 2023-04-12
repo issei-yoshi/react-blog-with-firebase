0bc4d650acf668e6b918e401e96d8c93e36dcffb
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-12 13:55:29 +0900

    Navbarにfontawesomeをimportして、コンポーネントとして使用

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFilePen, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
 import "./Navbar.css"
 
 function Navbar() {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/createpost">Post</Link>
-      <Link to="/login">Login</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        Home
+      </Link>
+      <Link to="/createpost">
+      <FontAwesomeIcon icon={faFilePen} />
+        Post
+      </Link>
+      <Link to="/login">
+      <FontAwesomeIcon icon={faArrowRightToBracket} />
+        Login
+      </Link>
     </nav>
   )
 }
