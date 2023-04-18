31070716645af9e2cbffed1d27cfe7ade8c3b6c5
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-18 15:43:17 +0900

    Login.jsにonClickイベントでloginInWithGoogle関数を発火させるように記載

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,14 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react'
+import { auth, provider } from '../firebase';
 
 function Login() {
+
   return (
-    <div>Login</div>
+    <div>
+      <p>Login</p>
+      <button onClick={loginInWithGoogle}>Login with Google</button>
+    </div>
   )
 }
 
