762b6ce6e601bf815daedaf888d90b91295ce2a5
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-18 15:53:58 +0900

    loginInWithGoogle関数の成功時にルートディレクトリに遷移するよう、useNavigateを使用

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,18 @@
 import { signInWithPopup } from 'firebase/auth';
 import React from 'react'
 import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 function Login({ setIsAuth }) {
+
+  const navigate = useNavigate();
 
   const loginInWithGoogle = () => {
     //Googleでログイン
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem("isAuth", true);
       setIsAuth(true);
+      navigate("/");
     });
   };
 
