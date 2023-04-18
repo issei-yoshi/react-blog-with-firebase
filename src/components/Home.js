2f1b4000ef95eee27fc604f8c0686e88af89645a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 06:51:35 +0900

    Home.jsでuseStateを用いて状態管理をし、更新用関数でfiresotoreから取得したデータを更新

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import { async } from '@firebase/util'
 import { collection, getDocs } from 'firebase/firestore'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { db } from '../firebase'
 import "./Home.css"
 
 function Home() {
+  const [postList, setPostList] = useState([]);
+
   useEffect(() => {
     const getPosts = async() => {
       const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
     };
     getPosts();
   }, []);
