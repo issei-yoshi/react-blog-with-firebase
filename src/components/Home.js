43c97581cb9f2a623892e8480fc747c1c14ae043
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 06:48:46 +0900

    Home.jsでuseEffectを用いてfirestoreからデータを取得

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import { async } from '@firebase/util'
+import { collection, getDocs } from 'firebase/firestore'
+import React, { useEffect } from 'react'
+import { db } from '../firebase'
 import "./Home.css"
 
 function Home() {
+  useEffect(() => {
+    const getPosts = async() => {
+      const data = await getDocs(collection(db, "posts"));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className='homePage'>
       <div className='postContents'>
