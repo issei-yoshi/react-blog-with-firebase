2b5588c3df34b7fb668adcaad8bd1d9fea389e0a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-18 17:01:21 +0900

    Home.jsでハードコーディング後、Home.cssでレイアウト調整

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className='postContents'>
+        <div className='postHeader'>
+          <h1>Title</h1>
+        </div>
+        <div className='postTextContainer'>
+          Learning React now dayo
+        </div>
+        <div className='nameAndDeleteButton'>
+          <h3>@Issei</h3>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
