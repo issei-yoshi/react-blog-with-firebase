37990576cea501512d3ae556855e027b6125fda1
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-18 16:12:51 +0900

    CreatePost.jsにてhtmlを記述して、cssを作成

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./CreatePost.css"
 
 function CreatePost() {
   return (
-    <div>CreatePost</div>
+    <div className='createPostPage'>
+      <div className='postContainer'>
+        <h1>Post Article</h1>
+        <div className='inputPost'>
+          <div>Title</div>
+          <input type="text" placeholder='put title' />
+        </div>
+        <div className='inputPost'>
+          <div>Post</div>
+          <textarea placeholder='put post content'></textarea>
+        </div>
+        <button className='postButton'>Post</button>
+      </div>
+    </div>
   )
 }
 
